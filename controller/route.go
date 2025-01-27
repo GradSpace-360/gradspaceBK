c0d9ee7abd775a6848e658d2e12344310a5c9196
07SUJITH <tssujith2002@gmail.com>
2025-01-27 21:36:26 +0530

    feat(cors): allow PATCH method in API routes

--- controller/route.go
+++ controller/route.go
@@ -9,7 +9,7 @@ func SetupRouter(app *fiber.App) {
 	base_api := app.Group("/api/v1")
 	base_api.Use(cors.New(cors.Config{
 		AllowOrigins:     "http://localhost:5173",
-		AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS",
+		AllowMethods:     "GET,POST,PUT,DELETE,PATCH,OPTIONS",
 		AllowHeaders:     "Origin,Content-Type,Accept,Authorization",
 		AllowCredentials: true,
 	}))
